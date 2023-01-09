fa1ec7bb51bb32354090d3deff18656691cf4885
a.seltsov <a.seltsov@azoft.com>
2023-01-09 15:16:21 +0700

    убрала точки с запятой

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import reportWebVitals from './reportWebVitals';
-import store from "./redux/redux-store";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
+import reportWebVitals from './reportWebVitals'
+import store from "./redux/redux-store"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import {Provider} from "react-redux"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <Provider store={store}>
@@ -14,6 +14,6 @@ root.render(
     </Provider>
 )
 
-reportWebVitals();
+reportWebVitals()
 
 
